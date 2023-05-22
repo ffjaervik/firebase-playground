32e0fe044684872705e2235b6a01b3b057ee5ec1
ffjaervik <fabianfjaervik@gmail.com>
2023-05-22 12:41:02 +0200

    read and write

---
+++ dbConfig.js
@@ -0,0 +1,12 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCTDsWW7WaWCmvkhDaz6TN-Yh1EFXERA0c",
+    authDomain: "fir-tutorial-fd6bc.firebaseapp.com",
+    projectId: "fir-tutorial-fd6bc",
+    storageBucket: "fir-tutorial-fd6bc.appspot.com",
+    messagingSenderId: "632965069761",
+    appId: "1:632965069761:web:981fff137ff67445ff4d40",
+    measurementId: "G-798CFTNPET",
+  };
+
+    export default firebaseConfig;  
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,82 @@
+import "./App.css";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import {
+  collection,
+  addDoc,
+  getDocs,
+  setDoc,
+  doc,
+  Timestamp,
+} from "firebase/firestore";
+import firebaseConfig from "../dbConfig";
+import { useState } from "react";
 
-import './App.css'
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// console.log("This is app" + app.options.apiKey)
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+// console.log("This is DB" + db.app.options.apiKey)
 
 function App() {
-  
+  const updateUser = async () => {
+    try {
+      const docRef = await addDoc(collection(db, "users"), {
+        author: author,
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  };
+
+  // Add a new document in collection "cities"
+  async function addCity() {
+    const cityRef = doc(db, "cities", "BJ");
+    setDoc(
+      cityRef,
+      { name: "Beijung", state: "Xianping", country: "China" },
+      { merge: true }
+    );
+  }
+  async function addTestData() {
+    const docData = {
+      stringExample: "Hello world!",
+      booleanExample: true,
+      numberExample: 3.14159265,
+      dateExample: Timestamp.fromDate(new Date("December 10, 1815")),
+      arrayExample: [5, true, "hello"],
+      nullExample: null,
+      objectExample: {
+        a: 5,
+        b: {
+          nested: "foo",
+        },
+      },
+    };
+    const dataRef = doc(db,"data","one")
+    await setDoc(dataRef, docData);
+  }
+
+  async function getDbData() {
+    const querySnapshot = await getDocs(collection(db, "users"));
+    querySnapshot.forEach((doc) => {
+      console.log(`${doc.id} => ${doc.data()}`);
+    });
+  }
 
   return (
     <>
-     
+      <form>
+        <label>Author:</label>
+        <button onClick={updateUser}>Update</button>
+      </form>
+      <button onClick={getDbData}>Get DB Data</button>
+      <button onClick={addCity}>Add City</button>
+      <button onClick={addTestData}>Add Test Data</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
